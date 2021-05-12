e6499e778304d35ef887b5f3e72c9d62b116206e
Lukas Liesis <liesislukas@gmail.com>
2021-05-12 16:41:15 +0300

    bugfix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const {nanoid} = require('nanoid');
+const {customAlphabet} = require('nanoid');
 
 function getId(prefix, chars) {
   prefix = typeof prefix === 'string' ? prefix : '';
   chars = chars || '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_';
   // generate 1k IDs/second and you will need 2 billion years to have 1% probability of 1 collision
   // https://zelark.github.io/nano-id-cc/
-  return prefix + nanoid(chars, 24);
+  return prefix + (customAlphabet(chars, 24)());
 }
 
 module.exports = getId;
