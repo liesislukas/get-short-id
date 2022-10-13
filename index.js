5efd1ce337fb832f8a7fb29e1a2371f9d1ca8d3f
lukas <liesislukas@gmail.com>
2022-10-13 20:09:37 +0300

    201 with count

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const {customAlphabet} = require('nanoid');
+const {customAlphabet} = require("nanoid");
 
-function getId(prefix, chars) {
-  prefix = typeof prefix === 'string' ? prefix : '';
-  chars = chars || '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_';
+function getId({prefix, count, chars}) {
+  prefix = typeof prefix === "string" ? prefix : "";
+  chars = chars || "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
   // generate 1k IDs/second and you will need 2 billion years to have 1% probability of 1 collision
   // https://zelark.github.io/nano-id-cc/
-  return prefix + (customAlphabet(chars, 24)());
+  count = count || 24;
+  return prefix + (customAlphabet(chars, count)());
 }
 
 module.exports = getId;
