5366fdb904e129f414e7c5b264a773045b35e17f
lukas <lukas@littlesaas.com>
2023-01-23 16:20:34 +0200

    rewrite to typescript & v 3.0.7

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+export default function getId(options: {
+  /**
+   * @param {string} If prefix = "u-" IDs will be something like: u-a487t6grede...
+   * @description getId({ prefix: `u-${Date.now()}`, count: 14 })
+   * */
+  prefix?: string,
+  /**
+   * @param {number} How many chars to use. Default is 14
+   * @description 14
+   * */
+  count?: number,
+  /**
+   * @param {string} Chars to use. Default is A-z0-9_
+   * @description 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_
+   * */
+  chars?: string
+}): string
