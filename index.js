0b8a41da3db0b5c0100e7dc0a767b22be640f8ae
lukas <lukas@littlesaas.com>
2023-01-23 16:20:29 +0200

    rewrite to typescript & v 3.0.7

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const {customAlphabet} = require("nanoid");
-
-function getId({prefix, count, chars}) {
-  prefix = typeof prefix === "string" ? prefix : "";
-  chars = chars || "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
-  // generate 1k IDs/second and you will need 2 billion years to have 1% probability of 1 collision
-  // https://zelark.github.io/nano-id-cc/
-  count = count || 24;
-  return prefix + (customAlphabet(chars, count)());
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const nanoid_1 = require("nanoid");
+function getId(options) {
+    let prefix = options?.prefix || '';
+    let chars = options?.chars || '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_';
+    let count = options?.count || 24;
+    // some math to check probability of collision
+    // https://zelark.github.io/nano-id-cc/
+    return `${prefix}${(0, nanoid_1.customAlphabet)(chars, count)()}`;
 }
-
-module.exports = getId;
+exports.default = getId;
--- index.ts
+++ index.ts
@@ -1,9 +1,27 @@
-import {customAlphabet} from 'nanoid';
+import { customAlphabet } from "nanoid";
 
-export default function getId(options: { prefix?: string, count?: number, chars?: string }): string {
-  let prefix = options?.prefix || '';
-  let chars = options?.chars || '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_';
-  let count = options?.count || 24;
+export default function getId(options: {
+  /**
+   * @param {string} If prefix = "u-" IDs will be something like: u-a487t6grede...
+   * @description getId({ prefix: `u-${Date.now()}` })
+   * */
+  prefix?: string;
+  /**
+   * @param {number} How many chars to use. Default is 14
+   * @description 14
+   * */
+  count?: number;
+  /**
+   * @param {string} Chars to use. Default is A-z0-9_
+   * @description 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_
+   * */
+  chars?: string;
+}): string {
+  let prefix = options?.prefix || "";
+  let chars =
+    options?.chars ||
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+  let count = options?.count || 14;
 
   // some math to check probability of collision
   // https://zelark.github.io/nano-id-cc/
